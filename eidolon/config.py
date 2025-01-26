fb1bcb50fb6ca38bfa11803b1fb100193fc4f3b4
Matt Kelly <8226466+mathisto@users.noreply.github.com>
2025-01-25 19:37:21 -0500

    Depersonalize repo

--- eidolon/config.py
+++ eidolon/config.py
@@ -2,14 +2,14 @@ import os
 
 class Config:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    OLLAMA_API_URL = os.getenv("OLLAMA_BASE_URL", "http://100.108.91.106:11434")
+    OLLAMA_API_URL = os.getenv("OLLAMA_BASE_URL")
 
     # Assemble the database URL using an environment variable for the password
     DATABASE_HOST = "localhost"
     DATABASE_PORT = "5432"
     DATABASE_NAME = "eidolon"
-    DATABASE_USER = "mathisto"
-    DATABASE_PASSWORD = os.getenv("PG_MATHISTO_PASS")  # Retrieve password from environment
+    DATABASE_USER = "eidolon"
+    DATABASE_PASSWORD = os.getenv("EIDOLON_PG_PASS")  # Retrieve password from environment
     DB_URL = (
         f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@"
         f"{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
